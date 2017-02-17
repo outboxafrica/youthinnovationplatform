d730435f63daea9c3b89ecbcaed766a888c5f51a
Phillip Ahereza <pahereza@gmail.com>
2017-02-17 17:02:52 +0300

    data tables for users and the different projects

---
+++ projects/models.py
@@ -0,0 +1,122 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+
+class InvestmentCompany(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    industry = models.CharField(max_length=255)
+    support_stage = models.CharField(max_length=255)
+    url = models.URLField()
+    ticket_size = models.CharField(max_length=20)
+    logo = models.FileField(upload_to='static/logos/%Y/%m/%d/', blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class CommunityHub(models.Model):
+    organisation_name = models.CharField(max_length=200, blank=True)
+    hub_focus = models.TextField(blank=True)
+    preferred_industries = models.CharField(max_length=200, blank=True)
+    investment_stage = models.CharField(max_length=200, blank=True)
+    support_type = models.CharField(max_length=255, blank=True)
+    url = models.CharField(max_length=200, blank=True)
+    logo = models.FileField(upload_to='static/logos/%Y/%m/%d/', blank=True)
+
+    def __unicode__(self):
+        return self.organisation_name
+
+
+class Innovation(models.Model):
+    stage = models.CharField(max_length=1, blank=True)
+    name = models.CharField(max_length=50, blank=True)
+    description = models.TextField(blank=True)
+    url = models.CharField(max_length=200, blank=True)
+    service_pic = models.ImageField(upload_to='static/service_pics/%Y/%m/%d/',
+                                    blank=True)
+    service_videos = models.TextField(blank=True)
+    sectors = models.TextField(blank=True)
+    other_sectors = models.CharField(max_length=30, blank=True)
+    challenge_to_solve = models.TextField(blank=True)
+    challenge_faced = models.TextField(blank=True)
+    logo = models.ImageField(upload_to='static/logos/%Y/%m/%d', blank=True)
+
+    target_customers = models.TextField(blank=True)
+    market_size = models.TextField(blank=True)
+    customer_lessons = models.TextField(blank=True)
+    acquisition_plan = models.TextField(blank=True)
+    potential_competitors = models.TextField(blank=True)
+    competitive_advantage = models.TextField(blank=True)
+    business_differentiator = models.TextField(blank=True)
+    major_wrongs = models.TextField(blank=True)
+
+    revenue = models.TextField(blank=True)
+    monthly_costs = models.FileField(upload_to='static/uploads/%Y/%m/%d/', blank=True)
+    annual_costs = models.FileField(upload_to='static/uploads/%Y/%m/%d/', blank=True)
+
+    growth_ambitions = models.TextField(blank=True)
+    milestones = models.TextField(blank=True)
+
+    do_you_have_audited_books = models.CharField(max_length=50, blank=True)
+    total_sales = models.CharField(max_length=50, blank=True)
+    total_capital = models.CharField(max_length=50, blank=True)
+    expected_capital = models.CharField(max_length=50, blank=True)
+    capital_type = models.CharField(max_length=50, blank=True)
+    capital_use = models.CharField(max_length=50, blank=True)
+
+    test_plan = models.TextField(blank=True)
+    impact_measure_plan = models.TextField(blank=True)
+    show_success_plan = models.TextField(blank=True)
+
+    costs = models.TextField(max_length=50, blank=True)
+
+    do_really_well = models.TextField(blank=True)
+    key_resources = models.TextField(blank=True)
+    key_partners = models.TextField(blank=True)
+
+    problem_change = models.TextField(blank=True)
+    customer_change = models.TextField(blank=True)
+    planned_acquisition_plan = models.TextField(blank=True)
+    performance = models.TextField(blank=True)
+    test_learnings = models.TextField(blank=True)
+
+    monthly_cashflow = models.FileField(
+        upload_to='static/cashflows/%Y/%m/%d/',
+        blank=True)
+    income_statement = models.FileField(
+        upload_to='static/income_statements/%Y/%m/%d/',
+        blank=True)
+    published = models.BooleanField(default=False)
+
+    def get_logo(self):
+        try:
+            return self.logo.url
+        except:
+            return "/"
+
+    def audited_books(self):
+        try:
+            vals = ["No", "Yes"]
+            return vals[int(self.do_you_have_auditedbooks)]
+        except:
+            return "No"
+
+    def type_of_capital_sought(self):
+        try:
+            vals = ["", "Equity", "Grants", "Convertible Debt",
+                    "Commercial Debt (Banks)", "Soft Debt (Friends)"]
+            return vals[int(self.capital_type)]
+        except:
+            return ""
+
+        # Override the __unicode__() method to return out something meaningful!
+    def __unicode__(self):
+        return self.name
+
+    @property
+    def __str__(self):
+        return self.name
---
+++ users/models.py
@@ -0,0 +1,71 @@
+from __future__ import unicode_literals
+from django.contrib.auth.models import AbstractUser
+from YouthInnovPltfrm import settings
+
+from projects.models import InvestmentCompany, CommunityHub, Innovation
+
+from django.db import models
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    USERNAME_FIELD = 'email'
+    mobile = models.CharField()
+    gender = models.CharField(max_length=10)
+    country = models.CharField(max_length=50)
+    picture = models.ImageField(
+        upload_to='static/profile_images',
+        blank=True,
+        default=settings.STATIC_URL + 'img/user-placeholder.png'
+    )
+    linkedin = models.URLField()
+    blog = models.URLField()
+    linkedin = models.CharField(max_length=200, blank=True)
+    twitter = models.CharField(max_length=200, blank=True)
+    summary = models.TextField()
+    age = models.IntegerField(blank=True)
+    timestamp = models.DateTimeField(auto_now=True)
+
+    def get_picture(self):
+            try:
+                return "/" + self.picture.url
+            except:
+                return "/"
+
+    def __str__(self):
+        return self.get_full_name()
+
+
+class Mentor(models.Model):
+    user = models.OneToOneField(User)
+    competencies = models.CharField(max_length=255, blank=True)
+    support_stage = models.Charfield()
+    support_type = models.CharField()
+    experience = models.CharField()
+
+
+class Investor(models.Model):
+    user = models.OneToOneField(User)
+    resume = models.FileField(upload_to='static/uploads/%Y/%m/%d/', blank=True)
+    company = models.ForeignKey(InvestmentCompany)  # to the investment company table
+
+
+class Innovator(models.Model):
+    user = models.OneToOneField(User)
+    experience = models.CharField()
+    team = models.ForeignKey(Innovation)  # to the innovation table
+
+
+class ProgramManager(models.Model):
+    user = models.OneToOneField(User)
+
+
+class HubManager(models.Model):
+    user = models.OneToOneField(User)
+    hub = models.ForeignKey(CommunityHub)  # to the community hub table
+
+
+
+
+
