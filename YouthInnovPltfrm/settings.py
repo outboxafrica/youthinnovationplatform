3723c77e75e5d6a438a92eec82a8e5e9192f6c97
Phillip Ahereza <pahereza@gmail.com>
2017-02-20 10:04:49 +0300

    configured heroku settings

--- YouthInnovPltfrm/settings.py
+++ YouthInnovPltfrm/settings.py
@@ -143,7 +143,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 
