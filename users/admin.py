a618f436cd4e0d0a5a8b9648f95f42fd35a989fc
Phillip Ahereza <pahereza@gmail.com>
2017-03-13 14:58:58 +0300

    added users to admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from users.models import Innovator, Investor, HubManager, ProgramManager, Mentor
 
 # Register your models here.
+
+admin.site.register(Innovator)
+admin.site.register(Investor)
+admin.site.register(HubManager)
+admin.site.register(ProgramManager)
+admin.site.register(Mentor)
