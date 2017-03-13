33e2505d54daa6cfc433f9c082d0095ad173eb2c
Phillip Ahereza <pahereza@gmail.com>
2017-03-13 14:40:27 +0300

    added innovation to admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from projects.models import Innovation, InvestmentCompany, CommunityHub
 
 # Register your models here.
+admin.site.register(Innovation)
+admin.site.register(InvestmentCompany)
+admin.site.register(CommunityHub)
