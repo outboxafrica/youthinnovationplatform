bed81498ccf77bb181cfeed7dda40b392f0ae21c
Phillip Ahereza <pahereza@gmail.com>
2017-03-29 14:54:06 +0300

    capitalize user role edit profile

--- users/views.py
+++ users/views.py
@@ -79,9 +79,11 @@ class ViewProfile(TemplateView):
 
 
 def view_profile(request):
+    user = User.objects.get(pk=request.user.id)
+    return render(request, 'users/my_profile.html', {'userprofile': user})
 
-    if not request.user.has_created_entity:
-        return HttpResponseRedirect(reverse('projects:select-startup-stage'))
-    else:
-        user = User.objects.get(pk=request.user.id)
-        return render(request, 'users/my_profile.html', {'userprofile': user})
+    # if not request.user.has_created_entity:
+    #     return HttpResponseRedirect(reverse('projects:select-startup-stage'))
+    # else:
+    #     user = User.objects.get(pk=request.user.id)
+    #     return render(request, 'users/my_profile.html', {'userprofile': user})
